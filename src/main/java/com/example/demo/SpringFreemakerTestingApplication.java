f1dda2abbba87cea5a88f39d07eab777289efd32
myonaingwinn <myonaingwin.gic@gmail.com>
2022-09-20 09:37:17 +0630

    init state

---
+++ src/main/java/com/example/demo/SpringFreemakerTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFreemakerTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFreemakerTestingApplication.class, args);
+	}
+
+}
